3ff412026d054753e669be84c1334d234e8a83f6
Tuan Phan <baotuanwork@gmail.com>
2025-11-26 13:21:00 +0200

    Time: 63 ms (21.81%), Space: 18.2 MB (20.93%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.py
+++ 1580-shuffle-the-array/1580-shuffle-the-array.py
@@ -1,11 +1,19 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        res = []
+        first, second = 0, n
+        maxVal = 10000
 
-        for i in range(0, len(nums)//2):
-            res.append(nums[i])
-            res.append(nums[n])
-            n += 1
+        for i in range(2*n):
+            if i % 2 == 0:
+                nums[i] = (nums[first] % maxVal) *  maxVal + nums[i]
+                first += 1
+            else:
+                nums[i] = (nums[second] % maxVal) * maxVal + nums[i]
+                second += 1
 
-        return res
+        for i in range(2*n):
+            nums[i] = nums[i] // maxVal
+        
+        return nums
+
         
