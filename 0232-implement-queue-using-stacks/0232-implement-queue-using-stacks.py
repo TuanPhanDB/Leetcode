605707420d56bd0f5e69f9daf1fffa8dadd4fd41
Tuan Phan <baotuanwork@gmail.com>
2025-12-13 12:40:23 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,20 +1,31 @@
-class MyQueue(object):
+class MyQueue:
+
     def __init__(self):
-        self.in_stk = []
-        self.out_stk = []
-	# Push element x to the back of queue...
-    def push(self, x):
-        self.in_stk.append(x)
-	# Remove the element from the front of the queue and returns it...
-    def pop(self):
-        self.peek()
-        return self.out_stk.pop()
-	# Get the front element...
-    def peek(self):
-        if not self.out_stk:
-            while self.in_stk:
-                self.out_stk.append(self.in_stk.pop())
-        return self.out_stk[-1]
-	# Return whether the queue is empty...
-    def empty(self):
-        return not self.in_stk and not self.out_stk
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x: int) -> None:
+        self.s1.append(x)
+
+    def pop(self) -> int:
+        while not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2.pop()
+
+    def peek(self) -> int:
+        while not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2[-1]
+
+    def empty(self) -> bool:
+        return max(self.s1, self.s2) == 0
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
